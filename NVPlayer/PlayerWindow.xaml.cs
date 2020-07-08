ddccd2ea937599ad1e1c2c3cc85f532c0392c210
靳兆鲁 <1756404649@qq.com>
2020-07-08 23:36:48 +0800

    update

---
+++ NVPlayer/PlayerWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace NVPlayer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class PlayerWindow : Window
+    {
+        public PlayerWindow()
+        {
+            InitializeComponent();
+           
+        }
+    }
+}
